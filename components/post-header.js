1b9d8ab0d4c24a96cae68e2912edb7a5835bc1c5
Daniel Shi <danielshi@Daniels-MBP.lan>
2023-09-28 14:49:57 -0700

    Adding project header

--- components/post-header.js
+++ components/post-header.js
@@ -1,14 +1,42 @@
-import Avatar from '../components/avatar'
-import CoverImage from '../components/cover-image'
-import PostTitle from '../components/post-title'
+import Avatar from "../components/avatar";
+import CoverImage from "../components/cover-image";
+import PostTitle from "../components/post-title";
 
-export default function PostHeader({ title, coverImage, date }) {
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BLOCKS } from "@contentful/rich-text-types";
+import markdownStyles from "./markdown-styles.module.css";
+import RichTextAsset from "./rich-text-asset";
+
+const customMarkdownOptions = (projectHeaderDescription) => ({
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: (node) => (
+      <RichTextAsset
+        id={node.data.target.sys.id}
+        assets={projectHeaderDescription.links.assets.block}
+      />
+    ),
+  },
+});
+
+export default function PostHeader({
+  title,
+  coverImage,
+  date,
+  projectHeaderDescription,
+}) {
   return (
-    <>
-      <PostTitle>{title}</PostTitle>
-      <div className="mb-8 md:mb-16 sm:mx-0">
-        <CoverImage title={title} url={coverImage.url} />
+    <div className="max-w-2xl mx-auto">
+      <div className={markdownStyles["markdown"]}>
+        {documentToReactComponents(
+          projectHeaderDescription.json,
+          customMarkdownOptions(projectHeaderDescription)
+        )}
+
+        <PostTitle>{title}</PostTitle>
+        <div className="mb-8 md:mb-16 sm:mx-0">
+          <CoverImage title={title} url={coverImage.url} />
+        </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
