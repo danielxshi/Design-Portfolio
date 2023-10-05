b3365047712a34542965c7d6ab0f64fff48aade3
Daniel Shi <danielshi@Daniels-MBP.lan>
2023-10-05 04:13:35 -0700

    Adjusting headerimage

--- components/Image/contentful-header-image.js
+++ components/Image/contentful-header-image.js
@@ -5,16 +5,16 @@ import cn from "classnames";
 export default function ImageHolder({ title, url, slug }) {
   const image = (
     <ContentfulImage
-      //   width={500}
-      //   height={500}
+    //   width={500}
+    //   height={500}
       alt={`Cover Image for ${title}`}
-      fill={true}
+        fill={true}
       src={url}
     />
   );
 
   return (
-    <div style={{ position: "absolute" }} className="sm:mx-0">
+    <div className="">
       {slug ? (
         <Link
           style={{ position: "absolute" }}
