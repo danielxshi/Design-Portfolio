e90d71e8893710bace55037004bd332e0e8c9069
Daniel Shi <danielshi@Daniels-MBP.lan>
2023-10-24 16:20:10 -0700

    Removing ternary operator on slug

--- components/Button/NextButton.js
+++ components/Button/NextButton.js
@@ -10,30 +10,9 @@ export default function NextButton({ posts }) {
           style={{ backgroundColor: "tomato" }}
         ></div>
         {posts.map((post) => (
-          <Link href={post.slug}>{post.title}</Link>
+          <Link key={post.title} href={post.slug}>{post.title}</Link>
         ))}
       </div>
     </div>
   );
 }
-
-// import PostPreview from "../components/post-preview";
-
-// export default function MoreStories({ posts }) {
-//   return (
-//     <section className="more-stories min-w-fit min-h-fit col-start-1 col-span-full">
-//       <div className="post-preview-container grid grid-cols-1 md:grid-cols-2 gap-y-20 md:gap-y-32 mb-20">
-//         {posts.map((post) => (
-//           <PostPreview
-//             key={post.slug}
-//             title={post.title}
-//             coverImage={post.coverImage}
-//             slug={post.slug}
-//             excerpt={post.excerpt}
-//             projectType={post.projectType}
-//           />
-//         ))}
-//       </div>
-//     </section>
-//   );
-// }
