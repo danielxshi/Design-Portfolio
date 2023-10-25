0ebab012d32078c0e261b836b3f92933f2bf7fcf
Daniel Shi <danielshi@Daniels-MBP.lan>
2023-10-24 17:10:56 -0700

    Adding slug nav

--- pages/posts/slug-nav.js
+++ pages/posts/slug-nav.js
@@ -81,22 +81,22 @@ export const SlugNav = ({ showModal, setShowModal, content, morePosts }) => {
   );
 };
 
-export async function getStaticProps({ params, preview = false }) {
-  const data = await getPostAndMorePosts(params.slug, preview);
+// export async function getStaticProps({ params, preview = false }) {
+//   const data = await getPostAndMorePosts(params.slug, preview);
 
-  return {
-    props: {
-      preview,
-      post: data?.post ?? null,
-      morePosts: data?.morePosts ?? null,
-    },
-  };
-}
+//   return {
+//     props: {
+//       preview,
+//       post: data?.post ?? null,
+//       morePosts: data?.morePosts ?? null,
+//     },
+//   };
+// }
 
-export async function getStaticPaths() {
-  const allPosts = await getAllPostsWithSlug();
-  return {
-    paths: allPosts?.map(({ slug }) => `/posts/${slug}`) ?? [],
-    fallback: true,
-  };
-}
+// export async function getStaticPaths() {
+//   const allPosts = await getAllPostsWithSlug();
+//   return {
+//     paths: allPosts?.map(({ slug }) => `/posts/${slug}`) ?? [],
+//     fallback: true,
+//   };
+// }
