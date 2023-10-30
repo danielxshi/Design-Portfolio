da07ef94716f372f4cb4b8c15cd25fe179035dc4
Daniel Shi <danielshi@Daniels-MBP.lan>
2023-10-29 21:27:42 -0700

    Fixed margins

--- components/intro.js
+++ components/intro.js
@@ -4,7 +4,7 @@ import Contact from "./contact";
 export default function Intro() {
   return (
     <section className="intro-container">
-      <div className="col-span-4">
+      <div className="">
         <h1>
           THE HUB FOR MY CASE STUDIES
         </h1>
