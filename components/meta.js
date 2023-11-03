f193fa559bcb8ffe261de34fa35eb1f39c98cef6
Daniel Shi <danielshi@Daniels-MBP.lan>
2023-11-03 02:52:52 -0700

    Mute the react tag

--- components/meta.js
+++ components/meta.js
@@ -36,7 +36,7 @@ export default function Meta() {
         name="description"
         content={`DANIEL'S NEXT.JS WEB DESIGN PORTFOLIO`}
       />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      {/* <meta property="og:image" content={HOME_OG_IMAGE_URL} /> */}
     </Head>
   )
 }
